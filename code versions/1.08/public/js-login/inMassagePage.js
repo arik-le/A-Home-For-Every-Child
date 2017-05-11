6643b02fd3f5145cd190633787c8760fedeaf0a0
gaiMaimoni <gai97781@gmail.com>
2017-05-11 14:29:18 +0300

    version 1.08 uploded by gaiMaimoni

---
+++ code versions/1.08/public/js-login/inMassagePage.js
@@ -0,0 +1,50 @@
+var inMassagePage=function()
+{
+
+     var headLine={
+          inputSection:
+               '<div class="container">'+
+                  '<ul class="nav nav-tabs  col-xs-12">'+
+                        '<li class="active col-xs-4 tabInMass"><a data-toggle="tab" href="#home">נכנסות</a></li>'+
+                        '<li class="col-xs-4 tabInMass" ><a data-toggle="tab" href="#menu1">יוצאות</a></li>'+
+                        '<li class="col-xs-4 tabInMass" ><a data-toggle="tab" href="#menu1">שליחה</a></li>'+
+                  '</ul>'+
+               "</div>"+
+               '<div id="MassageList" class="container"></div>'
+     }
+     var createMassage=function(massageID)
+     {
+            return massage={
+            inputSection:
+                '<div class="row massage" id="'+massageID+'">'+             
+                            '<span class="glyphicon glyphicon-trash col-xs-2 trash" id="'+massageID+'trash"></span>'+
+                            '<div class="col-xs-2"></div>'+
+                            '<h5 class="topic col-xs-6" id="topic'+massageID+'"></h5>'+          
+                            '<span class="glyphicon glyphicon-envelope col-xs-1 envelope"></span>'+  
+                            '<div class="col-xs-1"></div>'+  
+                '</div>'
+            } 
+     }
+     var addMessage=function(topic,massageID)
+     {
+        $("#MassageList").append(createMassage(massageID).inputSection);
+        $("#topic"+massageID).append(topic);
+        $("#"+massageID+"trash").click(function(){
+          var str=event.target.id;
+          str=str.substring(0, str.length-5);
+          removeMassage(str);
+        });
+     }
+     var removeMassage=function(id)
+     {
+        // remove also from database 
+       $("#"+id).remove();
+
+     }
+     var initPage=function()
+     {
+            $("#body").html(headLine.inputSection);
+     }
+     return{addMessage:addMessage,initPage:initPage};
+
+}();
