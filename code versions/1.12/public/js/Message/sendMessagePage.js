3589823e95de83427a9e971cd7702ba5899eb05f
gaiMaimoni <gai97781@gmail.com>
2017-05-16 12:14:10 +0300

    version 1.12 uploded by gaiMaimoni

--- code versions/1.12/public/js/Message/sendMessagePage.js
+++ code versions/1.12/public/js/Message/sendMessagePage.js
@@ -5,15 +5,12 @@ var GenMsgPage={
                   '<div class="GenMsgText row">הודעה כללית </div>'+
                       '<div class="sendFacilty row">'+
                         '<div class="blank col-xs-3"></div>'+
-                    '<div class="dropdown">'+
-                        '<button class="btn btn-default dropdown-toggle  col-xs-6" type="button" id="faciltyManu" data-toggle="dropdown">בחר מועדונית<span class="caret"></span></button>'+
-                        '<div class="dropdown-menu  pull-left col-xs-6 facManuGen">'+
-                            '<li><a  href="#">מועדונית1</a></li>'+
-                            '<li><a  href="#">מועדונית2</a></li>'+
-                            '<li><a  href="#">מועדונית3</a></li>'+
-                            '<li><a  href="#">מועדונית4</a></li>'+
-                        '</div>'+
-                    '</div>'+
+                        '<select>'+
+                            '<option value="club1">מועדונית1</option>'+
+                            '<option value="club2">מועדונית2</option>'+
+                            '<option value="club3">מועדונית3</option>'+
+                            '<option value="club4">מועדונית4</option>'+
+                        '</select>'+
                     '<div class="blank col-xs-3"></div>'+
                 '</div>'+
                 '<div class="sendToRow row">'+
@@ -73,51 +70,41 @@ var GenMsgPage={
           inputSection:
                '<div class="container">'+
                   
-                  '<div class="GenMsgText row">הודעה פרטית </div>'+
-                  '<div class="sendFacilty row">'+
-                     '<div class="blank col-xs-3"></div>'+
-                    '<div class="dropdown">'+
-                        '<button class="btn btn-default dropdown-toggle  col-xs-6" type="button" id="faciltyManu" data-toggle="dropdown">בחר מועדונית<span class="caret"></span></button>'+
-                        '<div class="dropdown-menu  pull-left col-xs-6">'+
-                            '<li><a  href="#">מועדונית1</a></li>'+
-                            '<li><a  href="#">מועדונית2</a></li>'+
-                            '<li><a  href="#">מועדונית3</a></li>'+
-                            '<li><a  href="#">מועדונית4</a></li>'+
-                        '</div>'+
+                '<div class="GenMsgText row">הודעה פרטית </div>'+
+                '<div class="row">'+
+                    '<div class="col-xs-offset-3 col-xs-6">'+
+                        '<select class="form-control">'+
+                            '<option value="club1">מועדונית1</option>'+
+                            '<option value="club2">מועדונית2</option>'+
+                            '<option value="club3">מועדונית3</option>'+
+                            '<option value="club4">מועדונית4</option>'+
+                        '</select>'+
                     '</div>'+
-                    '<div class="blank col-xs-3"></div>'+
                 '</div>'+
                 '<div class="sendToRow row">'+
                     '<div class="blank col-xs-7"></div>'+
                     '<div class ="sendToFac col-xs-5">:שלח אל</div>'+
                 '</div>'+
-              
-                '<div class="usersDropdown row">'+
-
-                     '<div class="blank col-xs-2"></div>'+
-                        '<div class="dropdown">'+
-                            '<button class="btn btn-default dropdown-toggle  col-xs-3" type="button" id="faciltyManu" data-toggle="dropdown">רשימה <span class="caret"></span></button>'+
-                            '<div class="dropdown-menu  pull-left col-xs-6">'+
-                                '<li><a  href="#">1</a></li>'+
-                                '<li><a  href="#">2</a></li>'+
-                                '<li><a  href="#">3</a></li>'+
-                                '<li><a  href="#">4 </a></li>'+
-                            '</div>'+
-                        '</div>'+
-
-                        '<div class="blank col-xs-2"></div>'+
-
-                        '<div class="dropdown">'+
-                            '<button class="btn btn-default dropdown-toggle  col-xs-4" type="button" id="guideManu" data-toggle="dropdown"> בחר משתמש <span class="caret"></span></button>'+
-                            '<div class="dropdown-menu  pull-left col-xs-6">'+
-                                '<li><a  href="#">מדריך</a></li>'+
-                                '<li><a  href="#">הורה</a></li>'+
-                                '<li><a  href="#">מורה</a></li>'+
-                                '<li><a  href="#">עוס</a></li>'+
-                            '</div>'+
-                        '</div>'+
-                      
+             
+                '<div class="row">'+
+                    '<div class="col-xs-offset-1 col-xs-5">'+
+                        '<select class="form-control">'+
+                            '<option value="user1">1</option>'+
+                            '<option value="user1">2</option>'+
+                            '<option value="user1">3</option>'+
+                            '<option value="user1">4</option>'+
+                        '</select>'+
+                    '</div>'+
+                    '<div class="col-xs-5">'+  
+                        '<select  class="form-control">'+
+                                '<option value="type">הורה</option>'+
+                                '<option value="type">מורה</option>'+
+                                '<option value="type">מדריך</option>'+
+                                '<option value="type">עו"ס</option>'+
+                        '</select>'+
+                    '</div>'+
                 '</div>'+
+                
                  '<div class="msgSubject row">'+
                     '<div class="blank col-xs-6"></div>'+
                     '<div class ="sendToFacP col-xs-5">:נושא</div>'+
@@ -129,7 +116,7 @@ var GenMsgPage={
                    
                 '</div>'+
                 '<div class= "msg-info form-group-lg">'+
-                         '<textarea rows="12"  class="msgBody col-xs-11 " id="usr"  placeholder="...כתוב כאן"></textarea>'+
+                         '<textarea rows="10"  class="msgBody col-xs-11 " id="usr"  placeholder="...כתוב כאן"></textarea>'+
                 '</div>'+
                 '<div class= "MsgButtons" rows="4">'+
                     '<div class="blank col-xs-3"></div>'+
