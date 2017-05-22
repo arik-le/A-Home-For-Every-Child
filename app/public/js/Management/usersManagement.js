7887ff743895b69be92242c6046a42fe18e8af47
Arik Levy <ariklevy96@gmail.com>
2017-05-22 14:38:45 +0300

    testing push

--- app/public/js/Management/usersManagement.js
+++ app/public/js/Management/usersManagement.js
@@ -142,6 +142,100 @@ var usersManagement = function()
 
                     '</div>'
         }
+
+     //-------------------------------------------------------------------------------------------------
+    //         FOR DINAMIC EDITING  USER
+	
+       var EditUserPage={
+        inputSection:
+        '<div class="container">'+
+            	'<div class="row main">'+
+				'<div class="panel-heading">'+
+	               '<div class="panel-title text-center">'+
+	               		'<h1 class="title">עריכת/מחיקת משתמש</h1>'+
+	               		'<hr />'+
+	                '</div>'+
+	            '</div> '+
+
+            	'<div class="main-login main-center">'+
+					'<form class="form-horizontal" method="post" action="#">'+
+						
+						'<div class="form-group">'+
+							'<label for="name" class="cols-sm-2 controlLabel" >:שם פרטי</label>'+
+							'<div class="cols-sm-10">'+
+								'<div class="input-group">'+
+									'<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>'+
+									'<input type="text" class="form-control" name="name" id="UserPName"  placeholder="פלוני"/>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+
+						'<div class="form-group">'+
+							'<label for="UserLastName" class="cols-sm-2 controlLabel" id="formTxts">:שם משפחה</label>'+
+							'<div class="cols-sm-10">'+
+								'<div class="input-group">'+
+									'<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>'+
+									'<input type="text" class="form-control" name="UserLastName" id="UserLName"  placeholder="אלמוני"/>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+
+						'<div class="form-group">'+
+							'<label for="username" class="cols-sm-2 controlLabel">:שם משתמש</label>'+
+							'<div class="cols-sm-10">'+
+								'<div class="input-group">'+
+									'<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>'+
+									'<input type="text" class="form-control" name="username" id="username"  placeholder="שם משתמש כלשהו"/>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+
+                        '<div class="form-group">'+
+							'<label for="username" class="cols-sm-2 controlLabel">:סוג משתמש</label>'+
+							'<div class="cols-sm-10">'+
+								'<div class="input-group">'+
+									'<span class="input-group-addon"><i class="fa fa-slideshare" aria-hidden="true"></i></span>'+
+									'<select type="text" class="form-control">'+
+                                    '<option class="ptUser">הורה</option>'+
+                                    '<option class="GuUser">מדריך</option>'+
+                                    '<option class="SWUser">עו"ס</option>'+
+                                    '<option class="AdmUser">מנהל</option>'+
+                                    '</select>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+
+						'<div class="form-group">'+
+							'<label for="password" class="cols-sm-2 controlLabel">:סיסמא</label>'+
+							'<div class="cols-sm-10">'+
+								'<div class="input-group">'+
+									'<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>'+
+									'<input type="password" class="form-control" name="password" id="password"  placeholder="****"/>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+
+						'<div class="form-group">'+
+							'<label for="confirm" class="cols-sm-2 controlLabel">:אימות סיסמא</label>'+
+							'<div class="cols-sm-10">'+
+								'<div class="input-group">'+
+									'<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>'+
+									'<input type="password" class="form-control" name="confirm" id="confirm"  placeholder="****"/>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+
+						'<div class="form-group ">'+
+							'<button type="button" class="btn btn-primary btn-lg edit-button"  >סיום עריכה</button>'+
+                            '<button type="button" class="btn-danger btn-danger btn-lg  delete-button"  >מחיקת משתמש</button>'+
+						'</div>'+
+					'</form>'+
+				'</div>'+
+        '</div>'
+     }
+
+
+
     //-------------------------------------------------------------------------------------------------
     // Initial page for injecting the html components
     var addUser=function()
