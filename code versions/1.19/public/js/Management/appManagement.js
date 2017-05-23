3838ae14990cdc054f5497090847ac8cdab1a6cd
Roy Oknin <royok17@outlook.com>
2017-05-23 14:06:45 +0300

    roy change

---
+++ code versions/1.19/public/js/Management/appManagement.js
@@ -0,0 +1,37 @@
+var appManagement = function ()
+{
+    //-------------------------------------------------------------------------------------------------
+     // inside manu for management page 
+    var headLine={
+        inputSection:
+            '<div class="container">'+
+                '<ul class="nav nav-tabs  col-xs-12">'+
+                    '<li class="active col-xs-4 usersMng"><a data-toggle="tab" href="#home">משתמשים</a></li>'+
+                    '<li class="col-xs-4 clubHouseMng" ><a data-toggle="tab" href="#menu1">מועדוניות</a></li>'+
+                    '<li class="col-xs-4 childrenMng" ><a data-toggle="tab" href="#menu1" >ילדים</a></li>'+
+                '</ul>'+
+            '</div>'+
+            '<div id="ManagementList" class="container"></div>'
+    }
+    //-------------------------------------------------------------------------------------------------
+   
+
+
+
+//-------------------------------------------------------------------------------------------------
+// Initial page for injecting the html components
+
+     var initPage=function()
+     {
+            $("#body").html(headLine.inputSection);
+            $(".usersMng").click (usersManagement.initPage);
+            $(".clubHouseMng").click (clubhouseManagement.initPage);
+     }
+//-------------------------------------------------------------------------------------------------
+
+
+
+
+
+    return{initPage:initPage};
+}();
