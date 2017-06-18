205c2a86028ea16936ce18ea6f16893d2984135e
Roy Oknin <royok17@outlook.com>
2017-06-18 21:42:43 +0300

    a little bit design

--- app/public/objects/clubHouse.js
+++ app/public/objects/clubHouse.js
@@ -5,10 +5,6 @@ var clubHouse = function(name,address)
         this.ClubhouseDBkey;  
 		var create = function(name,address)
 		{
-			/*this.PTusersList = [];           // list of parent or teacher user list - DB keys
-       	 	this.childList = [];           // list of child info contains photo,name  
-        	this.guidesList = [];        // list of all the guides / social workers with permission 
-			*/
 			return{
 				
 				name:name,
