3551cb9a0801f13b783bdae9609e17d6a36b405c
Roy Oknin <royok17@outlook.com>
2017-06-21 14:16:56 +0300

    design

--- app/public/js/formPage.js
+++ app/public/js/formPage.js
@@ -177,52 +177,6 @@ var formPage=function()
 					  <th>ערך</th>
 					</tr>
 				  </thead>
-<<<<<<< HEAD
-				  <tbody>
-					<tr>
-					  <th scope="row">1</th>
-					  <td>האם ההתנהגות של הילד השתפרה בשבוע הנוכחי?</td>
-					  <td>1/5</td>
-					</tr>
-					<tr>
-					  <th scope="row">1</th>
-					  <td>האם ההתנהגות של הילד השתפרה בשבוע הנוכחי?</td>
-					  <td>2/10</td>
-					</tr><tr>
-					  <th scope="row">1</th>
-					  <td>האם ההתנהגות של הילד השתפרה בשבוע הנוכחי?</td>
-					  <td>3/5</td>
-					<tr>
-					  <th scope="row">1</th>
-					  <td>האם ההתנהגות של הילד השתפרה בשבוע הנוכחי?</td>
-					  <td>1/10</td>
-					</tr>
-					<tr>
-					  <th scope="row">1</th>
-					  <td>האם ההתנהגות של הילד השתפרה בשבוע הנוכחי?</td>
-					  <td>2/5</td>
-					</tr><tr>
-					  <th scope="row">1</th>
-					  <td>האם ההתנהגות של הילד השתפרה בשבוע הנוכחי?</td>
-					  <td>7/10</td>
-					<tr>
-
-				  </tbody>
-				</table>
-			</div>
-			
-			<div class="fromDeatails">
-				<h5 id="formLbl">מאת: הורה32</h5>
-				<h5 id="formLbl">ילד: ישראל ישראלי</h5>
-				<h5 id="formLbl">תאריך: 29/5/17</h5>
-			</div>
-			
-			<div id="group_btn">
-				<a id="close_btn" class="btn btn-success btn-sq-sm"><span class="glyphicon glyphicon-ok-circle"></span></a>
-				<a id="del_btn" class="btn btn-danger btn-sq-sm"><span class="glyphicon glyphicon-trash"></span></a>
-			</div>`;
-        $("#body").html(showExactForm);
-=======
 				  <tbody>`;
         var answers=keysArray(form.result);
         var ans;
@@ -253,7 +207,6 @@ var formPage=function()
                 </div>`;
             $("#body").html(showExactForm);
         });
->>>>>>> de147a76be2d7b06c149a69f8cd4ec47b426d5d2
     }
 //================================================================================================
     var p=function(s)
@@ -261,92 +214,89 @@ var formPage=function()
         console.log(s);
     }
 //================================================================================================
-<<<<<<< HEAD
-        var fillFrom = function(key)
-=======
-var fillFrom = function(key)
-{
-   firebase.database().ref('clubhouse/'+login.correntClub[0]+'/forms/'+key).once("value")
-    .then(function(data)
+    var fillFrom = function(key)
     {
-        var keys=[];
-        for(obj in data.be().questions )
-            keys.push(obj);
-        numOfQuestions=keys.length;
-        var form=data.val();
-        var str;
-        $("#body").html('');
-        $("#body").css("text-align", "right");
-
-        for(var i=0;i<keys.length;i++)
->>>>>>> de147a76be2d7b06c149a69f8cd4ec47b426d5d2
+    firebase.database().ref('clubhouse/'+login.correntClub[0]+'/forms/'+key).once("value")
+        .then(function(data)
         {
-        firebase.database().ref('clubhouse/'+login.correntClub[0]+'/forms/'+key).once("value")
-            .then(function(data)
+            var keys=[];
+            for(obj in data.be().questions )
+                keys.push(obj);
+            numOfQuestions=keys.length;
+            var form=data.val();
+            var str;
+            $("#body").html('');
+            $("#body").css("text-align", "right");
+
+            for(var i=0;i<keys.length;i++)
             {
-                var keys=[];
-                for(obj in data.be().questions )
-                    keys.push(obj);
-                var form=data.val();
-                var str;
-                $("#body").html('');
-                $("#body").html('<div id="allTitles">'+form.subject+'</div></p>');
-                $("#body").css("text-align", "right");
-
-                for(var i=0;i<keys.length;i++)
+            firebase.database().ref('clubhouse/'+login.correntClub[0]+'/forms/'+key).once("value")
+                .then(function(data)
                 {
-                    str='<div class="allQue" id="que_'+i+'">'+
-                            '<h4 class = "qstlbl" dir="rtl">'+form.questions[keys[i]].question+'</h4>'+
-                            '<form id="form_'+i+'"class="choiceList">'+
-                            '</form>'+
-                        '</div>'
+                    var keys=[];
+                    for(obj in data.be().questions )
+                        keys.push(obj);
+                    var form=data.val();
+                    var str;
+                    $("#body").html('');
+                    $("#body").html('<div id="allTitles">'+form.subject+'</div></p>');
+                    $("#body").css("text-align", "right");
 
-                    $("#body").append(str);
-                    for(var j=0;j<form.questions[keys[i]].numOfvalues;j++) 
+                    for(var i=0;i<keys.length;i++)
                     {
-                        str='<label class="j_lebal">'+(j+1)+'<br />'+ 
-                            '<input type="radio" name="select" value="'+(j+1)+'"/>'+
-                            '</label>';
-                        $("#form_"+i).append(str);  
+                        str='<div class="allQue" id="que_'+i+'">'+
+                                '<h4 class = "qstlbl" dir="rtl">'+form.questions[keys[i]].question+'</h4>'+
+                                '<form id="form_'+i+'"class="choiceList">'+
+                                '</form>'+
+                            '</div>'
+
+                        $("#body").append(str);
+                        for(var j=0;j<form.questions[keys[i]].numOfvalues;j++) 
+                        {
+                            str='<label class="j_lebal">'+(j+1)+'<br />'+ 
+                                '<input type="radio" name="select" value="'+(j+1)+'"/>'+
+                                '</label>';
+                            $("#form_"+i).append(str);  
+                        }
                     }
-                }
-            });
-            var btn='<a id="sendForm_btn" class="btn btn-success btn-md btn-block">שלח</a>';
-            $("body").append(btn);
-        }
-        var loadAllForms=function()
+                });
+                var btn='<a id="sendForm_btn" class="btn btn-success btn-md btn-block">שלח</a>';
+                $("body").append(btn);
+            }
+        });
+    }
+    var loadAllForms=function()
+    {
+        $('.Nav').collapse('hide');
+        firebase.database().ref('clubhouse/'+login.correntClub[0]+'/forms').once("value")
+        .then(function(data)
         {
-            $('.Nav').collapse('hide');
-            firebase.database().ref('clubhouse/'+login.correntClub[0]+'/forms').once("value")
-            .then(function(data)
+            var str="";
+            var forms=data.val();
+            var i=0;
+            $("#body").html("");
+            for(key in forms)
             {
-<<<<<<< HEAD
-                var str="";
-                var forms=data.val();
-                var i=0;
-                $("#body").html("");
-                for(key in forms)
+                var tempId ="form_"+i;
+                str='<a class="btn btn-sq-lg clubSquare" id="'+tempId+'">'+
+                '<i class="fa fa-clipboard fa-2x"></i><br/>'+forms[key].subject+'</a>';
+                $("#body").append(str);
+                mainPage.paintButton(i,tempId);
+                $("#form_"+(i++)).click(function()
                 {
-                    var tempId ="form_"+i;
-                    str='<a class="btn btn-sq-lg clubSquare" id="'+tempId+'">'+
-                    '<i class="fa fa-clipboard fa-2x"></i><br/>'+forms[key].subject+'</a>';
-                    $("#body").append(str);
-                    mainPage.paintButton(i,tempId);
-                    $("#form_"+(i++)).click(function()
-                    {
-                        fillFrom(key);
-                    });
-                    
-                }
+                    fillFrom(key);
+                });
+                
+            }
 
-            
-            });
-        }
         
-        var test=function(id)
-        {
-            fillFrom(id);
-        }
+        });
+    }
+        
+    var test=function(id)
+    {
+        fillFrom(id);
+    }
 
     var updateClubs=function()				//update the list of clubHouses
 	{		
@@ -361,69 +311,6 @@ var fillFrom = function(key)
             $("#clubhousesForm").append('<option value="allClubs">כל המועדוניות</option>');
         });
 	}
-=======
-                  str=  '<label class="j_lebal">'+(j+1)+'<br />'+ 
-                    '<input id="rbtn_'+i+'_'+j+'" type="radio" name="select" value="'+(j+1)+'"/>'+
-
-                    '</label>';
-                  $("#form_"+i).append(str);  
-            }
-        }
-
-        var btn='<a id="sendForm_btn" class="btn btn-success btn-lg btn-block">שלח</a>';
-        $("#body").append(btn);
-        $("#sendForm_btn").click(function()
-        {
-            sendForm(data.be().questions);
-            loadAllForms();
-        });
-    });
-    
-}
-var loadAllForms=function()
-{
-    $('.Nav').collapse('hide');
-    firebase.database().ref('clubhouse/'+login.correntClub[0]+'/forms').once("value")
-    .then(function(data)
-    {
-        var str="";
-        var forms=data.val();
-        var i=0;
-        var keys=Object.keys(forms);
-        $("#body").html("");
-        for(key in forms)
-        {  
-            var this_user_ans=false;
-            for(id in forms[key].result)
-                 if(forms[key].result[id].user=== login.correntUser[1])
-                        this_user_ans=true;
-            if(this_user_ans)
-                str=  '<div class="selectFormDivAns" id="form_'+i+'">';
-            else
-                str=  '<div class="selectFormDiv" id="form_'+i+'">';
-                       str+= '<label class="SFL">'+forms[key].subject+'</label>'+
-                    '</div>';
-              $("#body").append(str);
-              if(!this_user_ans)
-              {
-                  p(i);
-                $("#form_"+i).click(function(e)
-                {
-                    var id=e.target.id;
-                    id=id.substring(5,id.length);
-                    var keys=Object.keys(forms);
-                    corForm=keys[id];
-                    p(corForm);
-                    fillFrom(key);
-                });
-              }
-              i++;
-        }
-
-      
-    });
-}
->>>>>>> de147a76be2d7b06c149a69f8cd4ec47b426d5d2
 
 //================================================================================================
     var sendForm=function(questions)
@@ -449,15 +336,16 @@ var loadAllForms=function()
         var result={user:login.correntUser[1],answers:ans};
         firebase.database().ref('clubhouse/'+login.correntClub[0]+'/forms/'+corForm+'/result').push(result);
     }
+
     var test=function()
     {
-    firebase.database().ref('clubhouse/-Kn-BZInu3pPlaW11V7v/forms/-Kn6CV3USW-N-KeL1up2').once("value")
-    .then(function(data)
-    {
-        var form=data.val();
-        var user="DIgNUeLsWndpOtrvK49MUbS3rgg2";
-        showForm(form,user);
-    });
+        firebase.database().ref('clubhouse/-Kn-BZInu3pPlaW11V7v/forms/-Kn6CV3USW-N-KeL1up2').once("value")
+        .then(function(data)
+        {
+            var form=data.val();
+            var user="DIgNUeLsWndpOtrvK49MUbS3rgg2";
+            showForm(form,user);
+        });
     }
     return {
         create:create,showForm:showForm,
